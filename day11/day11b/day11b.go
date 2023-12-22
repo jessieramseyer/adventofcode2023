ff1cd6063f309b34ef6fcf564cc6a03657ed926f
jessieramseyer <ramseyer.jessie@gmail.com>
2023-12-22 18:39:50 -0500

    day 11b

---
+++ day11/day11b/day11b.go
@@ -0,0 +1,78 @@
+package day11b
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Point struct {
+	X int
+	Y int
+}
+
+func Run() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	galaxyMap := make(map[int]Point)
+	noColExpandMap := make(map[int]bool)
+
+	row := 0
+	galaxy_num := 0
+	line := ""
+	for scanner.Scan() {
+		line = scanner.Text()
+		seen := false
+		for col, element := range line {
+			if element == '#' {
+				seen = true
+				galaxy_num++
+				point := Point{X: col, Y: row}
+				galaxyMap[galaxy_num] = point
+				//if galaxy found, mark it so that we know which columns to expand later on
+				noColExpandMap[col] = true
+
+			}
+		}
+		if !seen {
+			//expanding universe, add another row
+			row += 999999
+		}
+		row++
+	}
+
+	//expand out columns
+	offset := 0
+	for k := 0; k < len(line); k++ {
+		if noColExpandMap[k] == true {
+			continue
+		}
+		for n := 1; n <= galaxy_num; n++ {
+			if galaxyMap[n].X > k+offset {
+				galaxyMap[n] = Point{X: galaxyMap[n].X + 999999, Y: galaxyMap[n].Y}
+			}
+		}
+		offset += 999999
+	}
+
+	total_distances := 0
+
+	for i := 1; i <= galaxy_num; i++ {
+		for j := i + 1; j <= galaxy_num; j++ {
+			if i == j {
+				break
+			}
+			total_distances += abs(galaxyMap[i].X-galaxyMap[j].X) + abs(galaxyMap[i].Y-galaxyMap[j].Y)
+		}
+	}
+	fmt.Println(total_distances)
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
--- day11/main.go
+++ day11/main.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 
 	"github.com/jessieramseyer/adventofcode2023/day11/day11a"
+	"github.com/jessieramseyer/adventofcode2023/day11/day11b"
 )
 
 func main() {
 	fmt.Print("Part A: ")
 	day11a.Run()
+	fmt.Print("Part B: ")
+	day11b.Run()
 }
